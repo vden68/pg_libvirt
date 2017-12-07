3f90ce7677cc844092997e92894f92a75d0875b9
v.denisov <v.denisov@postgrespro.ru>
2017-12-07 16:16:17 +0300

    allure 4

--- test/test_connect_to_domain_by_ssh.py
+++ test/test_connect_to_domain_by_ssh.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 __author__ = 'vden'
 
-import allure
+import pytest
 
-@allure.step('проверка')
+@pytest.allure.step('проверка')
 def test_connect_to_domain_by_ssh(app):
 
-    with allure.step('Проверка шага clone_name={0}'):
+    with pytest.allure.step('Проверка шага clone_name={clone_name}'):
         clone_name = app.img.clone_an_image()
     #clone_name = 'ubuntu1604-64-serv-tst-001-1116-0'
     #clone_name = 'centos6-pg96'
     #clone_name='linux-ubuntu-16.04-x86_64'
 
-    with allure.step('Старт домена clone_name={0}'):
+    with pytest.allure.step('Старт домена clone_name={clone_name}'):
         st_domain= app.img.start_image(name_image= clone_name)
     print("\n st_domain= ", st_domain)
     app.pgl_ssh.ip= st_domain.IP
