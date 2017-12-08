b71be73ac449573ea2e270716dc023ca4b922f77
v.denisov <v.denisov@postgrespro.ru>
2017-12-08 12:31:37 +0300

    08122017 allure 1

--- fixture/fixapp/image.py
+++ fixture/fixapp/image.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 __author__ = 'vden'
 import os
-#import libvirt
+import pytest
 from fixture.fixlib.lib import Lib_helper
 
 class Image_helper:
@@ -11,10 +11,13 @@ class Image_helper:
 
 
     def clone_an_image(self):
-        conn_open_kvm = self.flib.connection_open()
 
-        lib_domain = self.flib.domain_check_for_availability(check_domain=self.app.pgl_kvm.name_sourse_image,
-                                                             conn=conn_open_kvm)
+        with pytest.allure.step('Подключаемся к KVM'):
+            conn_open_kvm = self.flib.connection_open()
+
+        with pytest.allure.step('Проверяем на наличие образа %s' % self.app.pgl_kvm.name_sourse_image):
+            lib_domain = self.flib.domain_check_for_availability(check_domain=self.app.pgl_kvm.name_sourse_image,
+                                                                 conn=conn_open_kvm)
         if lib_domain is None:
             print('Cannot find image %s to be clone.' %self.app.pgl_kvm.name_sourse_image)
             exit(0)
--- test/test_install_postgres_from_the_repository.py
+++ test/test_install_postgres_from_the_repository.py
@@ -6,17 +6,13 @@ import pytest
 @pytest.allure.step('Установка Postgres из репозитория')
 def test_install_postgres_from_the_repository(app):
 
-    with pytest.allure.step('Клонирование образ %s ' % app.pgl_kvm.name_sourse_image):
+    with pytest.allure.step('Клонирование образа %s ' % app.pgl_kvm.name_sourse_image):
         clone_name = app.img.clone_an_image()
-    #clone_name = 'ubuntu1604-64-serv-tst-001-1116-0'
-    #clone_name = 'centos6-pg96'
-    #clone_name='linux-ubuntu-16.04-x86_64'
 
     with pytest.allure.step('Стартуем виртуальную машину %s' % clone_name):
         st_domain= app.img.start_image(name_image= clone_name)
 
-    print("\n st_domain= ", st_domain)
     app.pgl_ssh.ip= st_domain.IP
-    print(app.pgl_ssh)
 
-    app.sh.connection_to_the_domain_by_ssh(domain=st_domain)
+    with pytest.allure.step('Устанавливаем Postgres'):
+        app.sh.connection_to_the_domain_by_ssh(domain=st_domain)
