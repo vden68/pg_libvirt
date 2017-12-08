2397699efab3cf2b074b8def4d196108ffa3ceec
v.denisov <v.denisov@postgrespro.ru>
2017-12-08 12:14:35 +0300

    08122017

--- test/test_install_postgres_from_the_repository.py
+++ test/test_install_postgres_from_the_repository.py
@@ -3,17 +3,18 @@ __author__ = 'vden'
 
 import pytest
 
-@pytest.allure.step('проверка')
+@pytest.allure.step('Установка Postgres из репозитория')
 def test_install_postgres_from_the_repository(app):
 
-    with pytest.allure.step('Проверка шага clone_name={clone_name}'):
+    with pytest.allure.step('Клонирование образ %s ' % app.pgl_kvm.name_sourse_image):
         clone_name = app.img.clone_an_image()
     #clone_name = 'ubuntu1604-64-serv-tst-001-1116-0'
     #clone_name = 'centos6-pg96'
     #clone_name='linux-ubuntu-16.04-x86_64'
 
-    with pytest.allure.step('some operation for clone_name=%s' % clone_name):
+    with pytest.allure.step('Стартуем виртуальную машину %s' % clone_name):
         st_domain= app.img.start_image(name_image= clone_name)
+
     print("\n st_domain= ", st_domain)
     app.pgl_ssh.ip= st_domain.IP
     print(app.pgl_ssh)
