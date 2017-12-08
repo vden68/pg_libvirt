07b5845538dca6b425fd4751ab86e03dc36a92ba
v.denisov <v.denisov@postgrespro.ru>
2017-12-08 15:38:46 +0300

    08122017 allure 6

--- fixture/fixapp/ssh.py
+++ fixture/fixapp/ssh.py
@@ -17,15 +17,17 @@ class Ssh_helper:
 
     def connection_to_the_domain_by_ssh(self, domain):
 
-
         print('\n',domain.name(), domain.UUIDString(), domain.ID(), domain.IP)
+
         with pytest.allure.step('Подключаемся по SSH к %s  IP адрес %s' %(domain.name(), domain.IP)):
             self.sshh.do_connect(args=self.app.pgl_ssh)
 
         config_file = os.path.join(os.path.dirname(os.path.abspath(__file__))+self.app.pgl_kvm.path_meta_json)
         print(config_file)
-        with open(config_file) as f:
-            repo = json.load(f)
+
+        with pytest.allure.step('Считываем файл (json) %s' % config_file):
+            with open(config_file) as f:
+                repo = json.load(f)
 
 
         split_name= self.app.pgl_kvm.name_sourse_image.split('-')
@@ -36,9 +38,11 @@ class Ssh_helper:
         for steps_f in steps_system:
             steps = steps_f['install']
 
-            for step in steps:
-                print('\nstep=', step)
-                self.sshh.do_run(command="sudo sh -c '"+step+"'")
+            with pytest.allure.step('Выполняем install из meta.json' ):
+                for step in steps:
+                    print('\nstep=', step)
+                    with pytest.allure.step('step= sudo sh -c %s' % step):
+                        self.sshh.do_run(command="sudo sh -c '"+step+"'")
                 
 
 
