1089cfb87c471dff0673b3e1936cd2f95e458b0a
v.denisov <v.denisov@postgrespro.ru>
2018-12-20 12:09:31 +0300

    test

--- test/test_install_multimaster_from_the_repository.py
+++ test/test_install_multimaster_from_the_repository.py
@@ -12,7 +12,7 @@ def test_install_multimaster_from_the_repository(app, mmts, ssh_trans):
 
 
 
-    """
+
     with pytest.allure.step('Clone an image %s ' % app.pgl_kvm.name_source_image):
         clone_name = app.img.clone_an_image(image=app.pgl_kvm.name_source_image, clone_name=None)
 
@@ -34,9 +34,9 @@ def test_install_multimaster_from_the_repository(app, mmts, ssh_trans):
 
 
 
-    """
 
-    clone_name_new = "m-m-quick--linux--centos--7--x86_64--m--11--1219--0"
+
+    #clone_name_new = "m-m-quick--linux--centos--7--x86_64--m--11--1219--0"
     time.sleep(10)
 
 
