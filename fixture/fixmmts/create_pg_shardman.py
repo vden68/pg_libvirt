729e9f179a2e1c9c1263ef65e6246015584cd1c5
v.denisov <v.denisov@postgrespro.ru>
2019-02-21 15:50:29 +0300

    test

---
+++ fixture/fixmmts/create_pg_shardman.py
@@ -0,0 +1,162 @@
+__author__ = 'vden'
+
+from fixture.fixapp.image import Image_helper
+from model.pgl_ssh import Pgl_ssh
+
+from fixture.fixssh.ssh import Sshh_helper
+import time
+
+class Create_mmts_helper:
+
+    def __init__(self, mmts):
+        self.img = Image_helper(self)
+        self.mmts = mmts
+        self.sshh = Sshh_helper(self)
+
+
+    def clone_shardman_image(self, image=None, app=None):
+        conn = app.img.conn_open_kvm2()
+        x=0
+        for xm in self.mmts.mmts_data:
+            new_image = "shardman_node"+self.mmts.mmts_data[x].node_id+"--"+image[7:]
+            self.mmts.mmts_data[x].images=new_image
+            if app.img.check_for_availability(image=new_image, conn_open_kvm=conn) is None:
+                app.img.clone_an_image(image=image, clone_name=new_image)
+
+            st_image = app.img.start_image(name_image=new_image)
+            self.mmts.mmts_data[x].images_ip=st_image.IP
+            self.mmts.mmts_data[x].host = st_image.IP
+            # self.mmts.conn_mmts.conn_strings=self.mmts.conn_mmts.conn_strings+' dbname='+self.mmts.conn_mmts.dbname+\
+            #       " user="+self.mmts.conn_mmts.user+" host="+st_image.IP+" password="+self.mmts.conn_mmts.password+","
+
+            self.mmts.conn_mmts.conn_strings = self.mmts.conn_mmts.conn_strings + ' dbname=' + self.mmts.conn_mmts.dbname + \
+                                               " user=" + self.mmts.conn_mmts.user + " host=" + st_image.IP + " password=" + self.mmts.conn_mmts.password + ","
+
+            x=x+1
+
+        self.mmts.conn_mmts.conn_strings="'"+self.mmts.conn_mmts.conn_strings[:-1]+"'"
+        print(self.mmts.mmts_data)
+        print(self.mmts.conn_mmts)
+
+
+    def tuning_shardman(self, ssh_trans=None):
+        x = 0
+        for xm in self.mmts.mmts_data:
+
+            conn_ssh_str = Pgl_ssh(ip=xm.images_ip, username="test", password="TestPass1")
+
+            # tuning pg_hba.conf
+            ssh_command_str="sudo chmod  777 /var/lib/pgpro/ent-11/data/pg_hba.conf"
+            ssh_trans.ssh_trans_exec_command(conn_ssh_str=conn_ssh_str, ssh_command=ssh_command_str)
+
+            ssh_command_str=\
+                "sudo sed -i '$ a host     all        all        0.0.0.0\/0       md5' /var/lib/pgpro/ent-11/data/pg_hba.conf"
+            ssh_trans.ssh_trans_exec_command(conn_ssh_str=conn_ssh_str, ssh_command=ssh_command_str)
+
+            ssh_command_str=\
+                "sudo sed -i '$ a host     replication        mtmuser        0.0.0.0\/0       md5' /var/lib/pgpro/ent-11/data/pg_hba.conf"
+            ssh_trans.ssh_trans_exec_command(conn_ssh_str=conn_ssh_str, ssh_command=ssh_command_str)
+
+            ssh_command_str = \
+                "sudo sed -i '$ a host     mydb        mtmuser        0.0.0.0\/0       md5' /var/lib/pgpro/ent-11/data/pg_hba.conf"
+            ssh_trans.ssh_trans_exec_command(conn_ssh_str=conn_ssh_str, ssh_command=ssh_command_str)
+
+            # tuning pg_hba.conf
+            ssh_command_str = "sudo  chmod  777 /var/lib/pgpro/ent-11/data/postgresql.conf"
+            ssh_trans.ssh_trans_exec_command(conn_ssh_str=conn_ssh_str, ssh_command=ssh_command_str)
+
+            ssh_command_str = 'sudo  sed -i "$ a wal_level = logical"  /var/lib/pgpro/ent-11/data/postgresql.conf'
+            ssh_trans.ssh_trans_exec_command(conn_ssh_str=conn_ssh_str, ssh_command=ssh_command_str)
+
+            ssh_command_str = 'sudo  sed -i "$ a max_connections = 100"  /var/lib/pgpro/ent-11/data/postgresql.conf'
+            ssh_trans.ssh_trans_exec_command(conn_ssh_str=conn_ssh_str, ssh_command=ssh_command_str)
+
+            ssh_command_str = 'sudo sed -i "$ a max_prepared_transactions = 600"  /var/lib/pgpro/ent-11/data/postgresql.conf'
+            ssh_trans.ssh_trans_exec_command(conn_ssh_str=conn_ssh_str, ssh_command=ssh_command_str)
+
+            ssh_command_str = 'sudo sed -i "$ a max_worker_processes = 300"  /var/lib/pgpro/ent-11/data/postgresql.conf'
+            ssh_trans.ssh_trans_exec_command(conn_ssh_str=conn_ssh_str, ssh_command=ssh_command_str)
+
+            ssh_command_str = 'sudo sed -i "$ a listen_addresses = \'*\' "  /var/lib/pgpro/ent-11/data/postgresql.conf'
+            ssh_trans.ssh_trans_exec_command(conn_ssh_str=conn_ssh_str, ssh_command=ssh_command_str)
+
+            ssh_command_str = 'sudo sed -i "$ a wal_sender_timeout = 0 "  /var/lib/pgpro/ent-11/data/postgresql.conf'
+            ssh_trans.ssh_trans_exec_command(conn_ssh_str=conn_ssh_str, ssh_command=ssh_command_str)
+
+            ssh_command_str = 'sudo sed -i "$ a shared_preload_libraries = \'multimaster, pg_stat_statements, pg_pathman\' "  /var/lib/pgpro/ent-11/data/postgresql.conf'
+            ssh_trans.ssh_trans_exec_command(conn_ssh_str=conn_ssh_str, ssh_command=ssh_command_str)
+
+            ssh_command_str = 'sudo sed -i "$ a max_wal_senders = 10 "  /var/lib/pgpro/ent-11/data/postgresql.conf'
+            ssh_trans.ssh_trans_exec_command(conn_ssh_str=conn_ssh_str, ssh_command=ssh_command_str)
+
+            ssh_command_str = 'sudo sed -i "$ a max_replication_slots = 20 "  /var/lib/pgpro/ent-11/data/postgresql.conf'
+            ssh_trans.ssh_trans_exec_command(conn_ssh_str=conn_ssh_str, ssh_command=ssh_command_str)
+
+            # ssh_command_str = ('sudo sed -i "$ a multimaster.max_nodes = %s "  /var/lib/pgpro/ent-11/data/postgresql.conf' %
+            #                    (self.mmts.conn_mmts.max_nodes))
+            # ssh_trans.ssh_trans_exec_command(conn_ssh_str=conn_ssh_str, ssh_command=ssh_command_str)
+            #
+            # ssh_command_str = ('sudo sed -i "$ a multimaster.node_id = %s "  /var/lib/pgpro/ent-11/data/postgresql.conf' %
+            #                    (xm.node_id))
+            # ssh_trans.ssh_trans_exec_command(conn_ssh_str=conn_ssh_str, ssh_command=ssh_command_str)
+            #
+            # ssh_command_str = ('sudo sed -i "$ a multimaster.conn_strings = %s "  /var/lib/pgpro/ent-11/data/postgresql.conf' %
+            #                    (self.mmts.conn_mmts.conn_strings))
+            # ssh_trans.ssh_trans_exec_command(conn_ssh_str=conn_ssh_str, ssh_command=ssh_command_str)
+
+
+
+            ssh_command_str = \
+                'sudo -H -u postgres psql -c "CREATE USER mtmuser WITH SUPERUSER ENCRYPTED PASSWORD \'mtmuserpassword\';"'
+            ssh_trans.ssh_trans_exec_command(conn_ssh_str=conn_ssh_str, ssh_command=ssh_command_str)
+
+            print('xm.node_id=', xm.node_id)
+            if xm.node_id=='2':
+                print('True.xm.node_id=', xm.node_id)
+
+                ssh_command_str = 'sudo -H -u postgres mkdir /var/lib/pgpro/ent-11/cfs'
+                ssh_trans.ssh_trans_exec_command(conn_ssh_str=conn_ssh_str, ssh_command=ssh_command_str)
+
+                ssh_command_str = 'sudo -H -u postgres psql -c "CREATE TABLESPACE cfs location \'/var/lib/pgpro/ent-11/cfs\' with (compression=true);"'
+                ssh_trans.ssh_trans_exec_command(conn_ssh_str=conn_ssh_str, ssh_command=ssh_command_str)
+
+                ssh_command_str = 'sudo -H -u postgres psql -c "CREATE DATABASE mydb OWNER mtmuser TABLESPACE cfs;"'
+                ssh_trans.ssh_trans_exec_command(conn_ssh_str=conn_ssh_str, ssh_command=ssh_command_str)
+
+                ssh_command_str = 'sudo -H -u postgres psql -d mydb -c "set default_tablespace=cfs;"'
+                ssh_trans.ssh_trans_exec_command(conn_ssh_str=conn_ssh_str, ssh_command=ssh_command_str)
+            else:
+                print('False.xm.node_id=', xm.node_id)
+
+                ssh_command_str = 'sudo -H -u postgres psql -c "CREATE DATABASE mydb OWNER mtmuser;"'
+                ssh_trans.ssh_trans_exec_command(conn_ssh_str=conn_ssh_str, ssh_command=ssh_command_str)
+
+            ssh_command_str = 'sudo pg-setup service stop'
+            ssh_trans.ssh_trans_exec_command(conn_ssh_str=conn_ssh_str, ssh_command=ssh_command_str)
+
+            ssh_command_str = 'sudo pg-setup service start'
+            ssh_trans.ssh_trans_exec_command(conn_ssh_str=conn_ssh_str, ssh_command=ssh_command_str)
+            time.sleep(10)
+
+
+        time.sleep(20)
+
+
+        conn_ssh_str = Pgl_ssh(ip=self.mmts.mmts_data[0].images_ip, username="test", password="TestPass1")
+        ssh_command_str = 'sudo -H -u postgres psql -d mydb -c "CREATE EXTENSION multimaster;" '
+        ssh_trans.ssh_trans_exec_command(conn_ssh_str=conn_ssh_str, ssh_command=ssh_command_str)
+
+
+        time.sleep(30)
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ test/test_install_pg_shardman_from_the_repository.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+__author__ = 'vden'
+
+import pytest
+import time
+
+@pytest.allure.step('Установка Мультимастера из репозитория')
+def test_install_multimaster_from_the_repository(app, mmts, ssh_trans):
+
+
+
+
+
+    # with pytest.allure.step('Clone an image %s ' % app.pgl_kvm.name_source_image):
+    #     clone_name = app.img.clone_an_image(image=app.pgl_kvm.name_source_image, clone_name=None)
+    #
+    # with pytest.allure.step('Start image %s' % clone_name):
+    #     st_domain= app.img.start_image(name_image= clone_name)
+    #
+    # app.pgl_ssh.ip= st_domain.IP
+    #
+    # with pytest.allure.step('Install Postgres'):
+    #     app.sh.install_postgrespro(domain=st_domain, quick_install_mode=True)
+    #
+    # clone_name_new= 'quick--'+clone_name
+    # with pytest.allure.step('Rename the virtual machine %s to %s' % (clone_name, clone_name_new)):
+    #     app.img.rename_image(name_image=clone_name, name_image_new=clone_name_new)
+    #
+
+
+
+
+
+
+
+
+
+
+    clone_name_new = "m--linux--centos--7--x86_64--m--11--0215--0"
+    time.sleep(10)
+
+
+    with pytest.allure.step('Clone an image %s ' % clone_name_new):
+        mmts.create.clone_mmts_image(image=clone_name_new, app=app)
+
+    with pytest.allure.step('Tuning mmts '):
+        mmts.create.tuning_mmts(ssh_trans=ssh_trans)
+
+
+
+#   sed -i '$ a \\nhost     all        all        0.0.0.0\/0       md5' /var/lib/pgpro/ent-10/data/pg_hba.conf
+#   sed -i '$ a \\nhost     replication        repl        0.0.0.0\/0       trust' /var/lib/pgpro/ent-10/data/pg_hba.conf
+
+
+
+
+
+
+
