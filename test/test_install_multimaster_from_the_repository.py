a5dfb4914fc5580e0c0ab00c3f4abf7a6783c378
v.denisov <v.denisov@postgrespro.ru>
2019-03-14 10:46:33 +0300

    test

--- test/test_install_multimaster_from_the_repository.py
+++ test/test_install_multimaster_from_the_repository.py
@@ -10,7 +10,7 @@ def test_install_multimaster_from_the_repository(app, mmts, ssh_trans):
 
 
 
-    """
+
     with pytest.allure.step('Clone an image %s ' % app.pgl_kvm.name_source_image):
         clone_name = app.img.clone_an_image(image=app.pgl_kvm.name_source_image, clone_name=None)
 
@@ -25,7 +25,6 @@ def test_install_multimaster_from_the_repository(app, mmts, ssh_trans):
     clone_name_new= 'quick--'+clone_name
     with pytest.allure.step('Rename the virtual machine %s to %s' % (clone_name, clone_name_new)):
         app.img.rename_image(name_image=clone_name, name_image_new=clone_name_new)
-    """
 
 
 
@@ -36,7 +35,8 @@ def test_install_multimaster_from_the_repository(app, mmts, ssh_trans):
 
 
 
-    clone_name_new = "m--linux--centos--7--x86_64--m--11--0215--0"
+
+    #clone_name_new = "m--linux--centos--7--x86_64--m--11--0215--0"
     time.sleep(10)
 
 
