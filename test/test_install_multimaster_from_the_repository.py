c2dd0cebf6c0eb3c518471220bd3e54f02a47620
v.denisov <v.denisov@postgrespro.ru>
2019-03-18 14:24:31 +0300

    test

--- test/test_install_multimaster_from_the_repository.py
+++ test/test_install_multimaster_from_the_repository.py
@@ -36,7 +36,7 @@ def test_install_multimaster_from_the_repository(app, mmts, ssh_trans):
 
 
 
-    clone_name_new = "m--linux--centos--7--x86_64--m--11--0315--1"
+    clone_name_new = "m--linux--centos--7--x86_64--m--11--0315--2"
     time.sleep(10)
 
 
